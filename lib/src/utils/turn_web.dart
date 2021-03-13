40f8036bb6f20a48562675a9c83dc47ba8094332
Jean-Michel Gaud <wirelessjeano@gmail.com>
2021-03-13 18:05:12 +0200

    Fixed for flutter 2.0

--- lib/src/utils/turn_web.dart
+++ lib/src/utils/turn_web.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 Future<Map> getTurnCredential(String host, int port) async {
   var url = 'https://$host:$port/api/turn?service=turn&username=flutter-webrtc';
-    final res = await http.get(url);
+    final res = await http.get(Uri(path: url));
     if (res.statusCode == 200) {
       var data = json.decode(res.body);
       print('getTurnCredential:response => $data.');
